4a2e3cc6b1471988b475c6378c79987a795e0c48
Alline <alline_franciely@hotmail.com>
2022-03-31 21:17:00 -0300

    requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -10,7 +10,10 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { email } = this.props;
+    const { email, currencies } = this.props;
+    const metodos = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+    const categorias = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+
     return (
       <div>
         <header>
@@ -21,6 +24,61 @@ class Wallet extends React.Component {
           <p data-testid="total-field"> 0 </p>
           <p data-testid="header-currency-field"> BRL </p>
         </header>
+        <form>
+          <label htmlFor="valor">
+            Valor:
+            <input
+              id="valor"
+              name="valor"
+              type="number"
+              data-testid="value-input"
+            />
+          </label>
+          <label htmlFor="descricao">
+            Descrição:
+            <input
+              id="descricao"
+              name="descricao"
+              type="text"
+              data-testid="description-input"
+            />
+          </label>
+          <label htmlFor="moeda">
+            Moeda:
+            <select
+              name="moeda"
+              id="moeda"
+            >
+              {currencies.map((currency) => <option key={ currency }>{currency}</option>)}
+            </select>
+          </label>
+          <label htmlFor="metodos">
+            Método de pagamento:
+            <select
+              name="metodos"
+              id="metodos"
+              data-testid="method-input"
+            >
+              {metodos.map((metodo) => <option key={ metodo }>{metodo}</option>)}
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select
+              name="tag"
+              id="tag"
+              data-testid="tag-input"
+            >
+              {categorias.map((categoria) => <option key={ categoria }>{categoria}</option>)}
+            </select>
+          </label>
+          <button
+            data-testid="edit-button-save"
+            type="button"
+          >
+            Adicionar despesa
+          </button>
+        </form>
       </div>
 
     );
@@ -29,6 +87,7 @@ class Wallet extends React.Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  currencies: state.wallet.currencies,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -38,6 +97,7 @@ const mapDispatchToProps = (dispatch) => ({
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
   setCurrencies: PropTypes.func.isRequired,
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
